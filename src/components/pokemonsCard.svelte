88915856504d53a8105be15aa19b46700e14face
Maujin111 <maurial593@gmail.com>
2022-05-04 03:55:03 -0500

    add pokemon details

--- src/components/pokemonsCard.svelte
+++ src/components/pokemonsCard.svelte
@@ -1,8 +1,9 @@
 <script>
+    import {fade} from 'svelte/transition';
     export let pokemons;
 </script>
 
-<a class="pokemonsCard" href={`/pokemons/${pokemons.id}`}>
+<a class="pokemonsCard" href={`/pokemons/${pokemons.id}`} transition:fade>
     <img class="pokemonsCardImage" src={pokemons.image} alt={pokemons.name}>
     <h2>#{pokemons.id} - {pokemons.name}</h2>
 </a>
---
+++ src/routes/pokemons/[id].svelte
@@ -0,0 +1,24 @@
+<script context="module">
+    export async function load({params}){
+        const id = params.id;
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        const res = await fetch(url);
+        const pokemon = await res.json();
+        return {props:{pokemon}};
+    }
+</script>
+<script>
+    export let pokemon;
+    const weight = pokemon.weight / 10;
+    const height = pokemon.height / 10;
+</script>
+
+<div class="pokemon">
+    
+    <h1>{pokemon.name}</h1>
+    <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
+    <p>
+        <strong>Type:</strong> {pokemon.types.map(type => type.type.name).join(' - ')} | <strong>height:</strong> {height} M | <strong>weight:</strong> {weight} Kg
+    </p>
+
+</div>
